5c4dd588a6d7693749b9bb71997a67af8d8b8de3
Boris Okunskiy <i@savant.pro>
2014-04-09 09:59:58 +0400

    Even more tests.

--- test/reviewer.js
+++ test/reviewer.js
@@ -31,6 +31,27 @@ describe('SemiQuiz Reviewer', function() {
       assert.equal(review.input.sort().join(','), 'five,three,three,two');
     });
 
+    it('should distribute submitted values', function() {
+      var $ = cheerio.load(review.html.filled);
+      assert.equal($('#0966d970').is(':checked'), true);
+      assert.equal($('#c6b5dd62').is(':checked'), true);
+      assert.equal($('#253b07b6').is(':checked'), true);
+      assert.equal($('#c78c4352').is(':checked'), true);
+      $ = cheerio.load(review.html.review);
+      assert.equal($('#0966d970').is(':checked'), true);
+      assert.equal($('#c6b5dd62').is(':checked'), true);
+      assert.equal($('#253b07b6').is(':checked'), true);
+      assert.equal($('#c78c4352').is(':checked'), true);
+    });
+
+    it('should report errors in review markup', function() {
+      var $ = cheerio.load(review.html.review);
+      assert.equal($('#item-c78c4352').hasClass('error'), true);
+      assert.equal($('#item-253b07b6').hasClass('error'), false);
+      assert.equal($('#c-46b48f3e').hasClass('error'), false);
+      assert.equal($('#c-f02bdb2f').hasClass('error'), true);
+    });
+
   });
 
   describe('radioGroup', function() {
@@ -49,6 +70,26 @@ describe('SemiQuiz Reviewer', function() {
       });
       assert.equal(review.errorIds.length, 0);
     });
+
+    it('should distribute submitted values', function() {
+      var review = new Reviewer().compile(form, {
+        'c-7d1ccf68': '912a7ef6'
+      });
+      var $ = cheerio.load(review.html.filled);
+      assert.equal($('#912a7ef6').is(':checked'), true);
+      $ = cheerio.load(review.html.review);
+      assert.equal($('#912a7ef6').is(':checked'), true);
+    });
+
+    it('should report errors in review markup', function() {
+      var review = new Reviewer().compile(form, {
+        'c-7d1ccf68': '912a7ef6'
+      });
+      var $ = cheerio.load(review.html.review);
+      assert.equal($('#item-912a7ef6').hasClass('error'), true);
+      assert.equal($('#c-7d1ccf68').hasClass('error'), true);
+    });
+
   });
 
   describe('selectMenu', function() {
@@ -76,6 +117,35 @@ describe('SemiQuiz Reviewer', function() {
       var review = new Reviewer().compile(form);
       assert.equal(review.errorIds.length, 3);
     });
+
+    it('should distribute submitted values', function() {
+      var review = new Reviewer().compile(form, {
+        'c-1e6c41c2': '0184b652',
+        'c-a2ced4dc': '0e6f1cea',
+        'c-78562237': '6441f554'
+      });
+      var $ = cheerio.load(review.html.filled);
+      assert.equal($('#c-1e6c41c2').val(), '0184b652');
+      assert.equal($('#c-a2ced4dc').val(), '0e6f1cea');
+      assert.equal($('#c-78562237').val(), '6441f554');
+      $ = cheerio.load(review.html.review);
+      assert.equal($('#c-1e6c41c2').val(), '0184b652');
+      assert.equal($('#c-a2ced4dc').val(), '0e6f1cea');
+      assert.equal($('#c-78562237').val(), '6441f554');
+    });
+
+    it('should report errors in review markup', function() {
+      var review = new Reviewer().compile(form, {
+        'c-1e6c41c2': '0184b652',
+        'c-a2ced4dc': '0e6f1cea',
+        'c-78562237': '6441f554'
+      });
+      var $ = cheerio.load(review.html.review);
+      assert.equal($('#c-1e6c41c2').hasClass('error'), true);
+      assert.equal($('#c-a2ced4dc').hasClass('error'), false);
+      assert.equal($('#c-78562237').hasClass('error'), false);
+    });
+
   });
 
   describe('inputText', function() {
@@ -95,6 +165,17 @@ describe('SemiQuiz Reviewer', function() {
       assert.equal(review.input.sort().join(','), 'TWELVE,Two,one');
     });
 
+    it('should distribute submitted values', function() {
+      var $ = cheerio.load(review.html.filled);
+      assert.equal($('#c-1ac95b3d').val(), 'one');
+      assert.equal($('#c-c89111ad').val(), 'Two');
+      assert.equal($('#c-a95f7481').val(), 'TWELVE');
+      $ = cheerio.load(review.html.review);
+      assert.equal($('#c-1ac95b3d').val(), 'one');
+      assert.equal($('#c-c89111ad').val(), 'Two');
+      assert.equal($('#c-a95f7481').val(), 'TWELVE');
+    });
+
   });
 
   describe('inputText review', function() {
@@ -102,11 +183,25 @@ describe('SemiQuiz Reviewer', function() {
     var review = new Reviewer().compile(form, {
       'c-1ac95b3d': 'un',
       'c-c89111ad': 'deux',
-      'c-a95f7481': 'trois'
+      'c-a95f7481': 'douze'
     });
 
     it('should report errors', function() {
       assert.equal(review.errorIds.length, 3);
+    });
+
+    it('should compile shallow reviews', function() {
+      var $ = cheerio.load(review.html.filled);
+      assert.equal($('#c-1ac95b3d').html(), '<mark>un</mark>');
+      assert.equal($('#c-c89111ad').html(), '<mark>deux</mark>');
+      assert.equal($('#c-a95f7481').html(), '<mark>douze</mark>');
+    });
+
+    it('should compile detailed reviews', function() {
+      var $ = cheerio.load(review.html.review);
+      assert.equal($('#c-1ac95b3d').html(), '<ins>one</ins><del>un</del>');
+      assert.equal($('#c-c89111ad').html(), '<ins>two</ins><del>deux</del>');
+      assert.equal($('#c-a95f7481').html(), '<ins>twelve</ins><del>douze</del>');
     });
 
   });
