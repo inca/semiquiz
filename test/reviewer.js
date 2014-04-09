1c0634e9274cbc545b3247bb00fee76d37f92832
Boris Okunskiy <i@savant.pro>
2014-04-09 09:41:24 +0400

    More tests.

--- test/reviewer.js
+++ test/reviewer.js
@@ -4,7 +4,8 @@ var Reviewer = require('../lib/reviewer')
   , fs = require('fs')
   , path = require('path')
   , assert = require('assert')
-  , _ = require('underscore');
+  , _ = require('underscore')
+  , cheerio = require('cheerio');
 
 function loadSample(id) {
   var data = fs.readFileSync(path.join('test/samples/' + id + '.json'), 'utf-8');
@@ -13,30 +14,101 @@ function loadSample(id) {
 
 describe('SemiQuiz Reviewer', function() {
 
-  it('checkboxGroup', function() {
+  describe('checkboxGroup', function() {
     var form = loadSample('checkboxGroup');
     var params = {
       'c-46b48f3e': ['0966d970', 'c6b5dd62'],
       'c-f02bdb2f': ['253b07b6', 'c78c4352']
     };
     var review = new Reviewer().compile(form, params);
-    assert.equal(review.errorIds.length, 1);
-    assert.equal(review.input.length, 4);
-    assert.equal(review.input.sort().join(','), 'five,three,three,two');
+
+    it('should detect errors', function() {
+      assert.equal(review.errorIds.length, 1);
+    });
+
+    it('should report input values', function() {
+      assert.equal(review.input.length, 4);
+      assert.equal(review.input.sort().join(','), 'five,three,three,two');
+    });
+
   });
 
-  it('radioGroup', function() {
+  describe('radioGroup', function() {
     var form = loadSample('radioGroup');
-    var review = new Reviewer().compile(form, {
-      'c-7d1ccf68': '912a7ef6'
+
+    it('should detect incorrect answer', function() {
+      var review = new Reviewer().compile(form, {
+        'c-7d1ccf68': '912a7ef6'
+      });
+      assert.equal(review.errorIds.length, 1);
     });
-    assert.equal(review.errorIds.length, 1);
-    review = new Reviewer().compile(form, {
-      'c-7d1ccf68': '6129e461'
+
+    it('should detect correct answer', function() {
+      var review = new Reviewer().compile(form, {
+        'c-7d1ccf68': '6129e461'
+      });
+      assert.equal(review.errorIds.length, 0);
     });
-    assert.equal(review.errorIds.length, 0);
   });
 
+  describe('selectMenu', function() {
+    var form = loadSample('selectMenu');
 
+    it('should detect incorrect answer', function() {
+      var review = new Reviewer().compile(form, {
+        'c-1e6c41c2': '0184b652',
+        'c-a2ced4dc': '0e6f1cea',
+        'c-78562237': '6441f554'
+      });
+      assert.equal(review.errorIds.length, 1);
+    });
+
+    it('should detect correct answer', function() {
+      var review = new Reviewer().compile(form, {
+        'c-1e6c41c2': '2c60392f',
+        'c-a2ced4dc': '0e6f1cea',
+        'c-78562237': '6441f554'
+      });
+      assert.equal(review.errorIds.length, 0);
+    });
+
+    it('should treat empty form as incorrect', function() {
+      var review = new Reviewer().compile(form);
+      assert.equal(review.errorIds.length, 3);
+    });
+  });
+
+  describe('inputText', function() {
+    var form = loadSample('inputText');
+
+    var review = new Reviewer().compile(form, {
+      'c-1ac95b3d': 'one',
+      'c-c89111ad': 'Two',
+      'c-a95f7481': 'TWELVE'
+    });
+
+    it('should detect correct answer', function() {
+      assert.equal(review.errorIds.length, 0);
+    });
+
+    it('should report input values', function() {
+      assert.equal(review.input.sort().join(','), 'TWELVE,Two,one');
+    });
+
+  });
+
+  describe('inputText review', function() {
+    var form = loadSample('inputText');
+    var review = new Reviewer().compile(form, {
+      'c-1ac95b3d': 'un',
+      'c-c89111ad': 'deux',
+      'c-a95f7481': 'trois'
+    });
+
+    it('should report errors', function() {
+      assert.equal(review.errorIds.length, 3);
+    });
+
+  });
 
 });
